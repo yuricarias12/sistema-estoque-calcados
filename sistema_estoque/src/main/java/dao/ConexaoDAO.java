92d015aaa6e2e778bebf90f07766639b9767c3cc
Yuri Carias <yuristricken7@gmail.com>
2023-11-24 16:00:02 -0300

    Sistema em andamento

---
+++ sistema_estoque/src/main/java/dao/ConexaoDAO.java
@@ -0,0 +1,33 @@
+/*
+DAO PARA ACESSO AOS OBJETOS
+DTO PARA TRANSFERENCIA DE OBJETOS
+ */
+package dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Estudo
+ */
+public class ConexaoDAO {
+    
+    public Connection conexaoBD() {
+        
+        Connection conn = null;
+        
+        try {
+            
+            String url = "jdbc:mysql://localhost:3306/sistema_estoque?user=root&password=";
+            conn = DriverManager.getConnection(url);
+            
+        } catch (SQLException erro) {
+            JOptionPane.showMessageDialog(null, "ConexaoDAO" + erro.getMessage());
+        }
+        return conn;
+    }
+    
+}
---
+++ sistema_estoque/src/main/java/dao/FuncionarioDAO.java
@@ -0,0 +1,146 @@
+
+package dao;
+
+import dto.FuncionarioDTO;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
+
+
+public class FuncionarioDAO {
+    
+    Connection conn;
+    PreparedStatement pstm;
+    ResultSet rs;
+    ArrayList<FuncionarioDTO> lista = new ArrayList<>();
+            
+    public void cadastrarFuncionario(FuncionarioDTO objfuncionariodto) {
+        
+        String sql = "INSERT INTO funcionario (matricula, senha, nome, cpf, rg, cargo, cep, bairro, cidade) VALUES (?, ? ,? ,? ,?, ?, ?, ?, ?)";
+        
+        conn = new ConexaoDAO().conexaoBD();
+        
+        try {
+            
+            pstm = conn.prepareStatement(sql);
+            pstm.setString(1, objfuncionariodto.getMatricula());
+            pstm.setString(2, objfuncionariodto.getSenha());
+            pstm.setString(3, objfuncionariodto.getNome());
+            pstm.setString(4, objfuncionariodto.getCpf());
+            pstm.setInt(5, objfuncionariodto.getRg());
+            pstm.setString(6, objfuncionariodto.getCargo());
+             pstm.setString(7, objfuncionariodto.getCep());
+              pstm.setString(8, objfuncionariodto.getBairro());
+               pstm.setString(9, objfuncionariodto.getCidade());
+            
+            pstm.execute();
+            pstm.close();
+            
+        } catch (SQLException erro) {
+            
+            JOptionPane.showMessageDialog(null, "FuncionarioDAO" + erro);
+        } 
+        
+    }
+    
+    //ResultSet usando o datatable percorre o SQL e trabalha com a parte dos atributos do banco de dados
+    
+    public ArrayList<FuncionarioDTO> PesquisarFuncionario() {
+        
+        String sql = "SELECT * FROM funcionario";
+        
+        conn = new ConexaoDAO().conexaoBD();
+        
+        try {
+            
+            pstm = conn.prepareStatement(sql);
+            rs = pstm.executeQuery();
+            
+            while (rs.next()) {
+                
+                FuncionarioDTO objfuncionarioDTO = new FuncionarioDTO();
+                
+                objfuncionarioDTO.setId_funcionario(rs.getInt("id_funcionario"));
+                objfuncionarioDTO.setMatricula(rs.getString("matricula"));
+                objfuncionarioDTO.setSenha(rs.getString("senha"));
+                objfuncionarioDTO.setNome(rs.getString("nome"));
+                objfuncionarioDTO.setCpf(rs.getString("cpf"));
+                objfuncionarioDTO.setRg(rs.getInt("rg"));
+                objfuncionarioDTO.setCargo(rs.getString("cargo"));
+                  objfuncionarioDTO.setCep(rs.getString("cep"));
+                    objfuncionarioDTO.setBairro(rs.getString("bairro"));
+                      objfuncionarioDTO.setCidade(rs.getString("cidade"));
+                
+                lista.add(objfuncionarioDTO);
+                
+            }
+            
+            
+        } catch (SQLException erro) {
+            
+            JOptionPane.showMessageDialog(null, "FuncionarioDAO Pesquisar" + erro);
+        }
+        
+        return lista;
+        
+        
+    }
+    
+    
+    public void AlterarFuncionario(FuncionarioDTO objfuncionariodto) {
+        
+        String sql = "UPDATE funcionario SET matricula = ?, senha = ?, nome = ?, cpf = ?, rg = ?, cargo = ?, cep = ?, bairro = ?, cidade = ? WHERE id_funcionario = ? ";
+        
+        
+         conn = new ConexaoDAO().conexaoBD();
+        
+        try {
+            
+            pstm = conn.prepareStatement(sql);
+            pstm.setString(1, objfuncionariodto.getMatricula());
+            pstm.setString(2, objfuncionariodto.getSenha());
+            pstm.setString(3, objfuncionariodto.getNome());
+            pstm.setString(4, objfuncionariodto.getCpf());
+            pstm.setInt(5, objfuncionariodto.getRg());
+            pstm.setString(6, objfuncionariodto.getCargo());
+            pstm.setString(7, objfuncionariodto.getCep());
+            pstm.setString(8, objfuncionariodto.getBairro());
+            pstm.setString(9, objfuncionariodto.getCidade());
+            pstm.setInt(10, objfuncionariodto.getId_funcionario());
+            
+            pstm.execute();
+            pstm.close();
+            
+        } catch (SQLException erro) {
+            
+            JOptionPane.showMessageDialog(null, "FuncionarioDAO Alterar" + erro);
+        } 
+        
+    }
+    
+    
+    public void excluirFuncionario(FuncionarioDTO objfuncionariodto) {
+        
+        String sql = "DELETE FROM funcionario WHERE id_funcionario = ?";
+        
+        conn = new ConexaoDAO().conexaoBD();
+        
+        try {
+            
+            pstm = conn.prepareStatement(sql);
+            
+            pstm.setInt(1, objfuncionariodto.getId_funcionario());
+            
+            pstm.execute();
+            pstm.close();
+            
+        } catch (SQLException erro) {
+            
+            JOptionPane.showMessageDialog(null, "FuncionarioDAO Excluir" + erro);
+        } 
+    }
+    
+}
